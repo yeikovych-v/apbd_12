6b97eb03facafa04d768d87cdc9d10dfc1acea6c
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-15 17:44:21 +0200

    Dumped all classes into one.

--- APBD_12/Program.cs
+++ APBD_12/Program.cs
@@ -1,41 +1,384 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using APBD_12.Data;
-using APBD_12.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<APBD_12Context>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("APBD_12Context") ?? throw new InvalidOperationException("Connection string 'APBD_12Context' not found.")));
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddDbContext<APBD_12Context>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("APBD_12Context") ??
+                                 throw new InvalidOperationException("Connection string 'APBD_12Context' not found.")));
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+        builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+        var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
 
-    SeedData.Initialize(services);
-}
+            SeedData.Initialize(services);
+        }
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{titleName?}");
+
+        app.Run();
+    }
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
 
-app.UseRouting();
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
 
-app.UseAuthorization();
+public class Movie
+{
+    public int Id { get; set; }
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{titleName?}");
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string? Title { get; set; }
 
-app.Run();
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
+    public string? Genre { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
+    public string? Rating { get; set; }
+}
+
+public class MovieGenreViewModel
+{
+    public List<Movie>? Movies { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }
+    public string? SearchString { get; set; }
+}
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new APBD_12Context(
+                   serviceProvider.GetRequiredService<
+                       DbContextOptions<APBD_12Context>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return; // DB has been seeded
+            }
+
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Rating = "R",
+                    Price = 7.99M
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Rating = "SS",
+                    Price = 8.99M
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Rating = "SS",
+                    Price = 9.99M
+                },
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Rating = "S",
+                    Price = 3.99M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
+
+namespace APBD_12.Data
+{
+    public class APBD_12Context : DbContext
+    {
+        public APBD_12Context(DbContextOptions<APBD_12Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; } = default!;
+    }
+}
+
+public class MoviesController : Controller
+{
+    private readonly APBD_12Context _context;
+
+    public MoviesController(APBD_12Context context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index(string movieGenre, string searchString)
+    {
+        if (_context.Movie == null)
+        {
+            return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+        }
+
+        // Use LINQ to get list of genres.
+        IQueryable<string> genreQuery = from m in _context.Movie
+            orderby m.Genre
+            select m.Genre;
+        var movies = from m in _context.Movie
+            select m;
+
+        if (!string.IsNullOrEmpty(searchString))
+        {
+            movies = movies.Where(s => s.Title!.Contains(searchString));
+        }
+
+        if (!string.IsNullOrEmpty(movieGenre))
+        {
+            movies = movies.Where(x => x.Genre == movieGenre);
+        }
+
+        var movieGenreVM = new MovieGenreViewModel
+        {
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+            Movies = await movies.ToListAsync()
+        };
+
+        return View(movieGenreVM);
+    }
+
+    [HttpPost]
+    public string Index(string searchString, bool notUsed)
+    {
+        return "From [HttpPost]Index: filter on " + searchString;
+    }
+
+    // GET: Movies/Details/5
+    public async Task<IActionResult> Details(int? titleName)
+    {
+        if (titleName == null)
+        {
+            return NotFound();
+        }
+
+        var movie = await _context.Movie
+            .FirstOrDefaultAsync(m => m.Id == titleName);
+        if (movie == null)
+        {
+            return NotFound();
+        }
+
+        return View(movie);
+    }
+
+    // GET: Movies/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Movies/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(movie);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(movie);
+    }
+
+    // GET: Movies/Edit/5
+    public async Task<IActionResult> Edit(int? titleName)
+    {
+        if (titleName == null)
+        {
+            return NotFound();
+        }
+
+        var movie = await _context.Movie.FindAsync(titleName);
+        if (movie == null)
+        {
+            return NotFound();
+        }
+
+        return View(movie);
+    }
+
+    // POST: Movies/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int titleName, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+    {
+        if (titleName != movie.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(movie);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(movie.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(movie);
+    }
+
+    // GET: Movies/Delete/5
+    public async Task<IActionResult> Delete(int? titleName)
+    {
+        if (titleName == null)
+        {
+            return NotFound();
+        }
+
+        var movie = await _context.Movie
+            .FirstOrDefaultAsync(m => m.Id == titleName);
+        if (movie == null)
+        {
+            return NotFound();
+        }
+
+        return View(movie);
+    }
+
+    // POST: Movies/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int titleName)
+    {
+        var movie = await _context.Movie.FindAsync(titleName);
+        if (movie != null)
+        {
+            _context.Movie.Remove(movie);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool MovieExists(int id)
+    {
+        return _context.Movie.Any(e => e.Id == id);
+    }
+}
+
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
+public class HelloWorldController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
